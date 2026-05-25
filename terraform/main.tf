33739d30984f6d769c5b1cc3012d39e9a9c867f1
Joao <joaodmorgadoribeiro@gmail.com>
2026-05-25 10:21:28 +0100

    feat: Running the Restaurancy Web

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,3 +35,18 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     load_balancer_sku = "standard"
   }
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "ireneandjoaoacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_role_assignment" "aks_acr" {
+  principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
--- terraform/ssh.tf
+++ terraform/ssh.tf
@@ -6,10 +6,10 @@ resource "azapi_resource" "ssh_public_key" {
 }
 
 resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
+  type                   = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  resource_id            = azapi_resource.ssh_public_key.id
+  action                 = "generateKeyPair"
+  method                 = "POST"
   response_export_values = ["publicKey", "privateKey"]
 }
 
