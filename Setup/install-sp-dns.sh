ac2b60d2e2d02c886c656abd3b2ec0d7f8e12610
IreneRomero <irene_romero_95@hotmail.com>
2026-05-26 16:34:43 +0200

    trying https

--- Setup/install-sp-dns.sh
+++ Setup/install-sp-dns.sh
@@ -30,6 +30,7 @@ echo "export DNS_CLIENT_SECRET=${CLIENT_SECRET}" >> /tmp/dns-sp-env.sh
 echo ""
 echo "==> Creating azuredns-config secret..."
 kubectl create secret generic azuredns-config \
+  --namespace external-dns \
   --from-literal=client-secret="${CLIENT_SECRET}" \
   --dry-run=client -o yaml | kubectl apply -f -
 
